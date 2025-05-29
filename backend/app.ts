09138daf27862a4e2efec1cf6afd0497a3870873
23kart <63331520+23kart@users.noreply.github.com>
2025-05-29 21:05:46 +0530

    user collection

--- backend/app.ts
+++ backend/app.ts
@@ -1,8 +1,10 @@
 import express from 'express';
 import cors from 'cors';
 import connectDB from './utils/db';
+
+//Routes
 import purchaseRoutes from './routes/purchaseRoutes';
-//router imports needed
+import userRoutes from './routes/userRoutes';
 
 const app = express();
 
@@ -12,6 +14,7 @@ app.use(express.json());
 
 //Routes
 app.use('/api', purchaseRoutes);
+app.use('/api', userRoutes);
 
 //Connect to Database
 connectDB();
---
+++ backend/routes/userRoutes.ts
@@ -0,0 +1,12 @@
+import express from "express";
+import {
+    createUser,
+    getAllUsers,
+} from "../controllers/userController";
+
+const router = express.Router();
+
+router.post('/users', createUser);
+router.get("/users", getAllUsers);
+
+export default router;
