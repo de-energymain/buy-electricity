aab3de6aa0e6dad8d7dc0e886feab5cd9cdad3b6
23kart <63331520+23kart@users.noreply.github.com>
2025-05-30 18:50:23 +0530

    delete user

--- backend/controllers/userController.ts
+++ backend/controllers/userController.ts
@@ -20,3 +20,22 @@ export const getAllUsers = async ( req: Request, res: Response) => {
         res.status(400).json({ message: (error as Error).message});
     }
 };
+
+export const deleteUser = async ( req: Request, res: Response) => {
+    try{
+        const { walletId } = req.params;
+        const deleteUser = await User.findOneAndDelete({
+            walletID : walletId
+        });
+        if ( !deleteUser ){
+           res.status(404).json({ message: "User not found."});
+           return;
+        }
+        res.status(200).json({
+            message: "User deletion successful.",
+            deleteUser
+        });
+    } catch (error) {
+        res.status(500).json({ message: (error as Error).message});
+    }
+};
